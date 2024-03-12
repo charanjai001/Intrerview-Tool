4266fb34913c86c3340d8fbe138f0c40f1ef92e8
charanjai001 <108685373+charanjai001@users.noreply.github.com>
2024-03-12 20:04:19 +0530

    Update connectMongoDB.js

--- connectMongoDB.js
+++ connectMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
  
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017", {
+    await mongoose.connect("mongodb://0.0.0.0:27017", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
